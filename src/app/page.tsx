593a6f714909031629237a74187a50531e96bdf2
= <renanacademic21@gmail.com>
2026-03-10 15:25:37 -0300

    opa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import { Navbar } from "@/components/sections/navbar";
 import { HeroSection } from "@/components/sections/hero";
-import { VideoBannerSection } from "@/components/sections/video-banner";
 import { HowItWorksSection } from "@/components/sections/how-it-works";
 import { ChallengeSection } from "@/components/sections/challenge";
 import { TeamSection } from "@/components/sections/team";
 import { AuthoritySection } from "@/components/sections/authority";
 import { EcosystemSection } from "@/components/sections/ecosystem";
 import { SponsorsSection } from "@/components/sections/sponsors";
-import { FormsSection } from "@/components/sections/forms";
 import { FooterSection } from "@/components/sections/footer";
 
 export default function Home() {
@@ -15,14 +13,12 @@ export default function Home() {
     <main className="relative">
       <Navbar />
       <HeroSection />
-      <VideoBannerSection />
       <HowItWorksSection />
       <ChallengeSection />
       <TeamSection />
       <AuthoritySection />
       <EcosystemSection />
       <SponsorsSection />
-      <FormsSection />
       <FooterSection />
     </main>
   );
